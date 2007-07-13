7d40892437d462bdf982a78de106f0f201c8338e
ywegoster <ywegoster@422d8ed4-0e34-0410-b212-ef1cdd1c6ea5>
2007-07-13 02:07:07 +0000

    ""

---
+++ trunk/Ensambladores/src/core/simbolos/SegmentoDatos.java
@@ -0,0 +1,24 @@
+/*
+ * SegmentoDatos.java
+ *
+ * Creado el 12 de julio de 2007, 21:04
+ *
+ * Autor: Victor Hugo Perez Alvarado
+ * Email: ywegoster@gmail.com
+ *
+ */
+
+package core.simbolos;
+
+/**
+ *
+ * @author Victor Hugo Perez Alvarado
+ */
+public class SegmentoDatos {
+    
+    private Vector<String> variables;
+    /** Crea una nueva instancia de SegmentoDatos */
+    public SegmentoDatos() {
+    }
+    
+}
