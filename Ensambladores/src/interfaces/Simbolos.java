d7ba1095fc033d193008ff8367865a3cd7d01d0a
ywegoster <ywegoster@422d8ed4-0e34-0410-b212-ef1cdd1c6ea5>
2007-07-13 02:06:51 +0000

    ""

---
+++ Ensambladores/src/interfaces/Simbolos.java
@@ -0,0 +1,24 @@
+/*
+ * Simbolos.java
+ *
+ * Created on 12 de julio de 2007, 20:55
+ *
+ */
+
+package interfaces;
+
+import java.util.Vector;
+
+/**
+ *
+ * @author Hugo
+ */
+public interface Simbolos {
+    
+    
+    
+    /**
+     * 
+     */
+    Vector<String> getDataSimbolos();
+}
