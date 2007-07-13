4e48358f9862c54a6ff4bab0569757a252c87035
ywegoster <ywegoster@422d8ed4-0e34-0410-b212-ef1cdd1c6ea5>
2007-07-13 02:07:07 +0000

    ""

---
+++ Ensambladores/src/core/simbolos/TablaSimbolos.java
@@ -0,0 +1,27 @@
+/*
+ * TablaSimbolos.java
+ *
+ * Creado el 12 de julio de 2007, 21:00
+ *
+ * Autor: Victor Hugo Perez Alvarado
+ * Email: ywegoster@gmail.com
+ *
+ */
+
+package core.simbolos;
+
+import java.util.Vector;
+
+/**
+ *
+ * @author Victor Hugo Perez Alvarado
+ */
+public class TablaSimbolos {
+    
+    private Vector<String> seudoInstrucciones;
+    
+    /** Crea una nueva instancia de TablaSimbolos */
+    public TablaSimbolos() {
+    }
+    
+}
