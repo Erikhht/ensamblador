4474a4f5e60fbebe8e52bea06a8685a1b97a5445
ywegoster <ywegoster@422d8ed4-0e34-0410-b212-ef1cdd1c6ea5>
2007-07-13 02:11:37 +0000

    ""

---
+++ trunk/Ensambladores/src/core/simbolos/variables/Variable.java
@@ -0,0 +1,24 @@
+/*
+ * Variable.java
+ *
+ * Creado el 12 de julio de 2007, 21:10
+ *
+ * Autor: Victor Hugo Perez Alvarado
+ * Email: ywegoster@gmail.com
+ *
+ */
+
+package core.simbolos.variables;
+
+/**
+ *
+ * @author Victor Hugo Perez Alvarado
+ */
+public class Variable {
+    
+    /** Crea una nueva instancia de Variable */
+    public Variable(String nombre, String tipo) {
+        
+    }
+    
+}
