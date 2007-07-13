f09dcff32ae1a109a33ff3f244e72749ecc8438a
franxogs <franxogs@422d8ed4-0e34-0410-b212-ef1cdd1c6ea5>
2007-07-13 02:36:56 +0000

    Variable

--- trunk/Ensambladores/src/core/simbolos/variables/Variable.java
+++ trunk/Ensambladores/src/core/simbolos/variables/Variable.java
@@ -10,17 +10,37 @@
 
 package core.simbolos.variables;
 
+import java.util.Vector;
+
 /**
  *
  * @author Victor Hugo Perez Alvarado
  */
 public class Variable {
     
+  private String name;
+  private String type;
+  private Vector value = new Vector();
+  private String direc;
     /** Crea una nueva instancia de Variable */
-    public Variable(String nombre, String tipo) {
-        
+    public Variable(String nombre, String tipo, Vector valor,String direccion) {
+        name=nombre;
+        type=tipo;
+        value=valor;
+        direc=direccion;
     }
     
-    public String getNombre();
-    public String getTipo();
+    public String getNombre(){
+        return name;
+    }
+    public String getTipo(){
+        return type;
+    }
+    public Vector getValue(){
+        return value;
+    }
+    public String getDireccion(){
+        return direc;
+    }         
+    
 }
