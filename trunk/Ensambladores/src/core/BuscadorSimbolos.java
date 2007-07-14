1c8c9838d821f8636b3a6c230f3d19380b167794
ywegoster <ywegoster@422d8ed4-0e34-0410-b212-ef1cdd1c6ea5>
2007-07-14 08:59:53 +0000

    ""

---
+++ trunk/Ensambladores/src/core/BuscadorSimbolos.java
@@ -0,0 +1,38 @@
+/*
+ * BuscadorSimbolos.java
+ *
+ * Created on 14 de julio de 2007, 02:43 AM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package core;
+
+import core.simbolos.TipoVariable;
+import core.simbolos.Variable;
+import excepciones.VariableNotFoundException;
+import java.util.Vector;
+
+/**
+ *
+ * @author Franxo
+ */
+public class BuscadorSimbolos {
+    
+    private Vector<Variable> variables;
+    
+    /**
+     * Creates a new instance of BuscadorSimbolos
+     */
+    public BuscadorSimbolos(Vector<Variable> var) {
+        this.variables = var;
+    }
+    
+    public Variable buscarVariable(String nombre) throws VariableNotFoundException{
+        for(Variable v : this.variables)
+            if(v.getNombre().equalsIgnoreCase(nombre))
+                return v;
+        throw new VariableNotFoundException("Variable no existente");
+    }
+}
