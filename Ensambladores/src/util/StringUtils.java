0b37ad228cc5e2fd258c155a9e930c6384290797
ywegoster <ywegoster@422d8ed4-0e34-0410-b212-ef1cdd1c6ea5>
2007-07-14 04:47:01 +0000

    ""

--- Ensambladores/src/util/StringUtils.java
+++ Ensambladores/src/util/StringUtils.java
@@ -18,6 +18,10 @@ import java.util.Vector;
  */
 public class StringUtils {
     
+   /**
+    * Toma un vecto de Strings y concatena cada una de sus elementos en un unico String
+    * en el se ingluyen un salto de linea excepto en la ultima linea
+    */
    public static String vectorToString(Vector<String> lineas){
        Object[] lineasCodigo = lineas.toArray();
        String codigo = "";
@@ -30,11 +34,28 @@ public class StringUtils {
        return codigo;
    }
    
+   /**
+    * Convierte un Vector de Strings en un arreglo de strings
+    */
    public static Vector<String> arrayToVector(String[] arreglo){
        Vector<String> vector = new Vector<String>();
        for(String a : arreglo)
            vector.add(a);
        return vector;
+   }
+   
+   /**
+    * Toma los elementos de un Vector y los almacena en un string, separados por una coma cada uno
+    */
+   public static String vectorToLinearString(Vector<String> lineas){
+       String linea = "";
+       for(int x = 0; x < lineas.size(); x++){
+           if(x == lineas.size() - 1)
+               linea += lineas.get(x);
+           else 
+               linea += lineas.get(x) + ", ";
+       }
+       return linea;
    }
     
 }
